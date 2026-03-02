5f26c96c63e984f15b7dff9027dde5732cb8b909
Lucas Silva <43251181+LucasSilvaPatricio@users.noreply.github.com>
2026-03-02 12:37:37 -0300

    update
    update

--- Vetores/src/Main.java
+++ Vetores/src/Main.java
@@ -1,5 +1,15 @@
 
 void main() {
+    double[]vect = new double[10];
+    double value = 0.0;
+    for(int i=0; i < vect.length; i++){
+        vect[i] = i;
+    }
 
+    for(int i = 0; i < vect.length; i++){
+        value += vect[i];
+    }
+
+    System.out.printf("Value: %.2f",value);
 
 }
