3b06704c6e87b861b238c877b9cd7107ba85bb4c
Lucas Silva <43251181+LucasSilvaPatricio@users.noreply.github.com>
2026-03-05 18:51:27 -0300

    update
    update

--- Vetores/src/Main.java
+++ Vetores/src/Main.java
@@ -1,15 +1,37 @@
+import entities.Product;
 
 void main() {
-    double[]vect = new double[10];
-    double value = 0.0;
-    for(int i=0; i < vect.length; i++){
-        vect[i] = i;
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    Product[] vect = new Product[n];
+
+    for(int k = 0; k < n; k++){
+        sc.nextLine();
+        String name = sc.nextLine();
+        double price = sc.nextDouble();
+        vect[k] = new Product(name, price);
     }
 
-    for(int i = 0; i < vect.length; i++){
-        value += vect[i];
+    double sum = 0.0;
+    for(int k = 0; k < vect.length; k++){
+        sum += vect[k].getPrice();
     }
 
-    System.out.printf("Value: %.2f",value);
+    double avg = sum/vect.length;
 
+    IO.println("AVERAGE PRICE: "+avg);
 }
+//    double[]vect = new double[10];
+//    double value = 0.0;
+//
+//    for(int i=0; i < vect.length; i++){
+//        vect[i] = i;
+//    }
+//
+//    for(int i = 0; i < vect.length; i++){
+//        value += vect[i];
+//    }
+//
+//    System.out.printf("Value: %.2f",value);
